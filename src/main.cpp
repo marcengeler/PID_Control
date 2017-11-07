64ded806e072acd571bd11ee8f17ec13d7674f73
Marc Engeler <marc.engeler@gmail.com>
2017-11-07 21:25:56 +0100

    tuning

--- src/main.cpp
+++ src/main.cpp
@@ -39,9 +39,8 @@ int main()
   // 0.3, 0.02, 10.0 still some oscillations in the signal, decided to
   // go down with the proportional part even more
   // Also decreased the integral part by a factor of 10, because the signal
-  // got worse over time, which indicates a too high integrational part, also
-  // slightly increased D part to countermeasure
-  pid.Init(0.15, 0.002, 20.0);
+  // got worse over time, which indicates a too high integrational part,
+  pid.Init(0.15, 0.02, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
