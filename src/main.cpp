9ec210a8e67cd9def0d7f6d64068924eafdf4a72
Marc Engeler <marc.engeler@gmail.com>
2017-11-07 21:53:04 +0100

    t

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ int main()
   
   // 0.2, 0.005, 21.0 looks ok, but has some large errors from time to time, so
   // I decresed the proportional part again
-  pid.Init(0.15, 0.001, 15.0);
+  pid.Init(0.15, 0.00015, 12.0);
   
   
   // Added PD Throttle Control
